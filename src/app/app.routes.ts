782d14016829224a1ba9887b68dbea5136c76859
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2025-05-14 11:10:54 -0500

    Configuracion de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CreateComponent } from './components/create/create.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ErrorComponent } from './components/error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: AboutComponent },
+    { path: 'sobre-mi', component: AboutComponent },
+    { path: 'proyectos', component: ProjectsComponent },
+    { path: 'crear-proyecto', component: CreateComponent },
+    { path: 'contacto', component: ContactComponent },
+    { path: '**', component: ErrorComponent }
+];
