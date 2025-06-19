5d185481ea9cb3fb653b77078366ecd1935220cc
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2025-06-19 08:55:18 -0500

    Implementacion de routerLinkActive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterLink, RouterOutlet, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
