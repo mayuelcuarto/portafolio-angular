a9645475d3e1fc5e953105c7222e0ecf6314771c
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2026-01-15 01:42:54 -0500

    Creacion de formulario de nuevos proyectos

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { Form, FormsModule } from '@angular/forms';
+import { Project } from '../../models/project';
+import { ProjectService } from '../../services/project.service';
 
 @Component({
   selector: 'app-create',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './create.component.html',
-  styleUrl: './create.component.css'
+  styleUrl: './create.component.css',
+  providers: [ProjectService]
 })
 export class CreateComponent {
+  public title?: string;
+  public project: Project = {
+    _id: '',
+    name: '',
+    description: '',
+    category: '',
+    year: 2025,
+    langs: '',
+    image: ''
+  }
+   
+  constructor(
+    private _projectService: ProjectService
+  ) {
+    this.title = "Crear Proyecto";
+  }
 
+  onSubmit(form?:Form){
+    console.log(this.project);
+  }
 }
