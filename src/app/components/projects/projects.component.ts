3eb94fc01735f4a8284fd137a88524689865f001
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2026-01-21 05:28:18 -0500

    Implementacion de listado de proyectos

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Project } from '../../models/project';
+import { ProjectService } from '../../services/project.service';
+import { Global } from '../../services/global';
 
 @Component({
   selector: 'app-projects',
+  standalone: true,
   imports: [],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.css'
+  styleUrl: './projects.component.css',
+  providers: [ProjectService]
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
+  public projects: Project[] = [];
+  public url?: string;
 
+  constructor(
+    private _projectService: ProjectService
+  ){
+    this.url = Global.url;
+  }
+
+  ngOnInit(){
+    this.getProjects();
+  }
+
+  getProjects(){
+    this._projectService.getProjects().subscribe({
+      next: (response) => {
+        if(response.projects){
+          this.projects = response.projects;
+        }
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }
 }
