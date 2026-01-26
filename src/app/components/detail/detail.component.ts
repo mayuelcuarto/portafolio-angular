5b1fe33a95c963f8e2402e10a9af11efe7820da7
Cristhian Mayuri Quiroz <cristhianmayuri@gmail.com>
2026-01-26 03:26:25 -0500

    Implementacion de metodos de consulta de detalle de proyecto

--- src/app/components/detail/detail.component.ts
+++ src/app/components/detail/detail.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Project } from '../../models/project';
+import { ProjectService } from '../../services/project.service';
+import { Global } from '../../services/global';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-detail',
   imports: [],
   templateUrl: './detail.component.html',
   styleUrl: './detail.component.css',
+  providers: [ProjectService]
 })
-export class DetailComponent {
+export class DetailComponent implements OnInit {
+  public url?: string;
+  public project?: Project;
 
+  constructor(
+    private _projectService: ProjectService,
+    private _router: Router,
+    private _route: ActivatedRoute
+  ){
+    this.url = Global.url;
+  }
+
+  ngOnInit(){
+    this._route.params.subscribe(
+      params => {
+        let id = params['id'];
+        this.getProject(id);
+      }
+    );
+  }
+
+  getProject(id: string){
+    this._projectService.getProject(id).subscribe({
+      next: (response) => {
+        this.project = response.project;
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }
 }
